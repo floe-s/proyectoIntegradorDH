29b3612a28243bfef3a3cae46910f027f3910efe
floe-s <fschettini@uade.edu.ar>
2022-09-08 22:15:36 -0300

    Agregue los enlaces a las distintas vistas, y cree el archivo app.js para simular el servidor en el puerto 3000

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/cursos.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/cursos.html'));
+});
+
+app.get('/descargables.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/descargables.html'));
+});
+
+app.get('/traductor.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/traductor.html'));
+});
+
+app.get('/suscripciones.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/suscripciones.html'));
+});
+
+app.get('/contacto.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/contacto.html'));
+});
