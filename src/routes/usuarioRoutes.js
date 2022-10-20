9fd9bc25a998b7a2f397992dc695bc5f305e22dd
floe-s <fschettini@uade.edu.ar>
2022-10-20 15:38:59 -0300

    trabajo con la vista y rutas de editar usuario

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -8,11 +8,11 @@ router.get('/registro',usuarioController.registro);
 router.post('/regitro', usuarioController.registrar);
 
 router.get('/login',usuarioController.login);
-router.post('/login2',usuarioController.logeado);
+router.post('/login',usuarioController.logeado); //esta ruta va?
 
 router.post('/perfil',usuarioController.perfil);
 
-router.get('/editarUsuario', usuarioController.editar);
-// router.put('/editarUsuario/:id', productosController.update);
+router.get('/editar-usuario', usuarioController.editar);
+router.put('/editar-usuario/:id', usuarioController.update);
 
 module.exports = router;
