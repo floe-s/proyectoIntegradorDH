8f662115e63672f665fa38410248cf94a2cc8c1b
floe-s <fschettini@uade.edu.ar>
2022-12-05 15:39:12 -0300

    agrego archivo .env y hago cambios en el config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
+   /*  "username": "root",
     "password": "root",
     "database": "moidih",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": 8889
+    "port": 8889 */
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
   },
   "test": {
     "username": "root",
@@ -22,3 +30,4 @@ module.exports = {
     "dialect": "mysql"
   }
 }
+
