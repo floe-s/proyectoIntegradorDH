9dbf40803f0fb5ece160a0b295e536121e5943a7
floe-s <fschettini@uade.edu.ar>
2022-12-05 17:57:19 -0300

    cambios config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,32 +2,33 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-   /*  "username": "root",
-    "password": "root",
-    "database": "moidih",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": 8889 */
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT,
     "port": process.env.DB_PORT
-  }
-  /* "test": {
-    "username": "root",
-    "password": "root",
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
   },
   "production": {
-    "username": "root",
-    "password": "root",
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  } */
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+  }
 }
+
+/*  "username": "root",
+    "password": "root",
+    "database": "moidih",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": 8889 */
 
