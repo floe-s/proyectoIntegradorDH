31d58b1501cb6e4995a647011c047f87d0786382
Camilo Andrade <andradecamilo226@gmail.com>
2022-12-06 01:03:22 -0500

    arreglos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
 
   "development": {
+    
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
